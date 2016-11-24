f2bd86cd17d5caa1f617d0f4c36c5920d0118f10
Alireza Josheghani <a.josheghani@anetwork.ir>
2016-11-24 19:48:53 +0330

    Add icon method

--- src/Menu.rb
+++ src/Menu.rb
@@ -1,7 +1,7 @@
 class Menu
 	
 	## setter and getter options
-	attr_accessor :name , :link , :title
+	attr_accessor :name , :link , :title , :icon
 
 	##
 	# set link option of item
@@ -23,7 +23,18 @@ class Menu
   # @return [Object]
   def title(title)
 		@title = title
-	end
+  end
+
+  ##
+  # set icon option of item
+  #
+  # @author Alireza Josheghani <a.josheghani@anetwork.ir>
+  # @since 24 Nov 2016
+  # @param [string] icon
+  # @return [Object]
+  def icon(icon)
+    @icon = icon
+  end
 
   ##
   # set route name
@@ -43,13 +54,13 @@ class Menu
 	# @since 24 Nov 2016
 	# @return [Object]
 	def make(item)
+
+    # set route name
     self.set_item_name(item)
 
-		{
-        :route => @route,
-				:title => @title,
-				:link => @link
-		}
+    # return render object
+		{ route: @route, title: @title, link: @link }
+
 	end
 	
 end
